dbd982e06f10b8ba01402686bcbd3a3e406c4ef9
Delano Ramdas <delanodzr@gmail.com>
2021-11-12 22:47:20 +0200

    fix: work through getting target output merged into player gameweek data

--- main.py
+++ main.py
@@ -11,6 +11,25 @@ def main():
     ##Enrich the data by adding in target values for each player
     #Create function to aggregate player data for gameweeks
     #Plug into shape
+    for position in positions:
+        #dataset = pd.read_csv(("data\\raw\\%s.csv" % positions[position]), sep=",", index_col='GW')
+        dataset = pd.read_csv("data\\by_player\\raw\\gks.csv", index_col='GW')
+
+    players = pd.unique(dataset['name'])
+    for player in players:
+        player_dataset = dataset[dataset['name'] == 'Aaron Ramsdale']
+        target_points = {'GW': range(1, 38), 'Target_Output': []}
+
+        for index, row in player_dataset.iterrows():
+            if index < 38:
+                target_points['Target_Output'].append(player_dataset.iloc[index]['total_points'])
+
+        target_player_dataset = player_dataset.copy().iloc[:-1, :]
+        target_player_dataset['Target_Output'] = target_points['Target_Output']
+    print(target_player_dataset[['total_points', 'Target_Output']])
+        ##THE PROBLEM IS THAT THE GW INDEX IS DUPLICATED FOR DOUBLE GAMEWEEKS
+       # player_dataset.to_csv('data\\%s\\%s.csv' % (positions[position], player))
+
 
 
     """   
