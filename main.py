56f15e236f7887d5f864aa332abfe17bfc4ed34d
Delano Ramdas <delanodzr@gmail.com>
2021-11-13 11:55:54 +0200

    enrich player csvs with target output

--- main.py
+++ main.py
@@ -8,27 +8,30 @@ import pandas as pd
 def main():
     positions = {'GK': 'gks', 'DEF': 'defs', 'MID': 'mids', 'FWD': 'fwds'}
 
-    ##Enrich the data by adding in target values for each player
-    #Create function to aggregate player data for gameweeks
     #Plug into shape
     for position in positions:
-        #dataset = pd.read_csv(("data\\raw\\%s.csv" % positions[position]), sep=",", index_col='GW')
-        dataset = pd.read_csv("data\\by_player\\raw\\gks.csv", index_col='GW')
+        dataset = pd.read_csv(("data\\by_player\\raw\\%s.csv" % positions[position]), sep=",", index_col='GW')
 
-    players = pd.unique(dataset['name'])
-    for player in players:
-        player_dataset = dataset[dataset['name'] == 'Aaron Ramsdale']
-        target_points = {'GW': range(1, 38), 'Target_Output': []}
+        players = pd.unique(dataset['name'])
+        for player in players:
+            player_dataset = dataset[dataset['name'] == player]
+            player_dataset = player_dataset.drop_duplicates(['kickoff_time'])
+            if player_dataset.shape[0] != 38:
+                continue
+            target_points = []
 
-        for index, row in player_dataset.iterrows():
-            if index < 38:
-                target_points['Target_Output'].append(player_dataset.iloc[index]['total_points'])
+            gameweek = 0
+            for index, row in player_dataset.iterrows():
+                gameweek += 1
+                if index < 38:
+                    target_points.append(player_dataset.iloc[gameweek]['total_points'])
 
-        target_player_dataset = player_dataset.copy().iloc[:-1, :]
-        target_player_dataset['Target_Output'] = target_points['Target_Output']
-    print(target_player_dataset[['total_points', 'Target_Output']])
-        ##THE PROBLEM IS THAT THE GW INDEX IS DUPLICATED FOR DOUBLE GAMEWEEKS
-       # player_dataset.to_csv('data\\%s\\%s.csv' % (positions[position], player))
+            target_player_dataset = player_dataset.copy().iloc[:-1, :]
+            try:
+                target_player_dataset['Target_Output'] = target_points
+            except:
+                print("oops" + player)
+            target_player_dataset.to_csv('data\\by_player\\%s\\%s.csv' % (positions[position], player))
 
 
 
